cdfe5128d257461caf63ec43cae96de10e9359cb
Luis Salles <107654852+LuisSallles@users.noreply.github.com>
2023-09-14 11:51:42 -0300

    14/09/2023

--- javascript.js
+++ javascript.js
@@ -1,8 +1,17 @@
-// Function to calculate the IMC
-function calculate() {
+function calculate() { // Function to calculate the IMC
     const name = String(document.getElementById('txtname').value)
     const height = Number(document.getElementById('txtheight').value)
     const weight = Number(document.getElementById('txtweight').value)
-    const imc = weight / (height * height)
     let result = document.getElementById('calculator-result')
+    const imc = weight / (height * height) // IMC calculate
+    if(imc < 18.5) { // Here it will be evaluated if the person has a good IMC
+        result.style.color = 'red'
+        result.innerHTML = `<p>${name}, your IMC is ${imc.toPrecision(2)} and you are underweigth! :(</p>`  // Under IMC
+    } else if (imc < 25) {
+        result.style.color = 'green'
+        result.innerHTML = `<p>${name}, your IMC is ${imc.toPrecision(2)} and you are healthy! :)</p>` // Good IMC
+    } else {
+        result.style.color = 'red'
+        result.innerHTML = `<p>${name}, your IMC is ${imc.toPrecision(2)} and you are overweigth! :(</p>` // Over IMC
+    } 
 }
