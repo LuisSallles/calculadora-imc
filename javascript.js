8995ca46699e8479b3d2613d9dd173f858f019e9
Luis Salles <107654852+LuisSallles@users.noreply.github.com>
2023-09-14 19:56:16 -0300

    .

--- javascript.js
+++ javascript.js
@@ -1,17 +1,35 @@
+function isData(n, h, w) { // Funtion to check if it has dates on input
+    if(n.length === 0 || h.value === 0 || w.value === 0) {
+        return false
+    } else {
+        return true
+    }
+}
+
 function calculate() { // Function to calculate the IMC
     const name = String(document.getElementById('txtname').value)
     const height = Number(document.getElementById('txtheight').value)
     const weight = Number(document.getElementById('txtweight').value)
     let result = document.getElementById('calculator-result')
     const imc = weight / (height * height) // IMC calculate
-    if(imc < 18.5) { // Here it will be evaluated if the person has a good IMC
-        result.style.color = 'red'
-        result.innerHTML = `<p>${name}, your IMC is ${imc.toPrecision(2)} and you are underweigth! :(</p>`  // Under IMC
-    } else if (imc < 25) {
-        result.style.color = 'green'
-        result.innerHTML = `<p>${name}, your IMC is ${imc.toPrecision(2)} and you are healthy! :)</p>` // Good IMC
+
+    if(isData(name, height, weight)) {
+        if(imc < 18.5) { // Here it will be evaluated if the person has a good IMC
+            result.style.color = 'red' // Under IMC
+            result.innerHTML = `<p>${name}, your IMC is ${imc.toPrecision(2)} and you are underweigth! :(</p>`
+        } else if (imc < 25) { 
+            result.style.color = 'green' // Good IMC
+            result.innerHTML = `<p>${name}, your IMC is ${imc.toPrecision(2)} and you are healthy! :)</p>`
+        } else {
+            result.style.color = 'red' // Over IMC
+            result.innerHTML = `<p>${name}, your IMC is ${imc.toPrecision(2)} and you are overweigth! :(</p>` 
+        } 
     } else {
-        result.style.color = 'red'
-        result.innerHTML = `<p>${name}, your IMC is ${imc.toPrecision(2)} and you are overweigth! :(</p>` // Over IMC
-    } 
+        result.style.color = 'white'
+        result.innerHTML = `<p>Fill in all the datas above!</p>`
+    }
+}
+
+function clearThis(target) { // Function to clean the inputs
+    target.value = ''
 }
